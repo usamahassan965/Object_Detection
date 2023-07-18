ebde628f4b26bc0b675bd235164ef7b753acad2f
Usama Hassan <96824810+usamahassan965@users.noreply.github.com>
2023-07-18 09:41:32 +0500

    Add files via upload
    Object Detection from Scratch

---
+++ Object_Detection_alladin/Non_max_suppression_pytorch.py
@@ -0,0 +1,50 @@
+##################################### Object Detection Course - aladdin Persson ########################################
+
+### Metric to remove overlap bounding boxes
+### First, start with discarding those bounding boxes have probability score (of containing object) is less than the threshold
+### Second, for remaining bounding boxes, do take out the bounding box with largest probability .
+### Then find IOU wiht the remaining boxes and remove those which are greater than some IOU threshold. Do this for all classes.
+
+
+import torch
+from IOU_pytorch import intersection_over_union
+
+def nms(bboxes, iou_threshold, threshold, box_format="corners"):
+    """
+    Does Non Max Suppression given bboxes
+
+    Parameters:
+        bboxes (list): list of lists containing all bboxes with each bboxes
+        specified as [class_pred, prob_score, x1, y1, x2, y2]
+        iou_threshold (float): threshold where predicted bboxes is correct
+        threshold (float): probability threshold to remove predicted bboxes (independent of IoU)
+        box_format (str): "midpoint" or "corners" used to specify bboxes
+
+    Returns:
+        list: bboxes after performing NMS given a specific IoU threshold
+    """
+
+    assert type(bboxes) == list
+
+    bboxes = [box for box in bboxes if box[1] > threshold]
+    bboxes = sorted(bboxes, key=lambda x: x[1], reverse=True)
+    bboxes_after_nms = []
+
+    while bboxes:
+        chosen_box = bboxes.pop(0)
+
+        bboxes = [
+            box
+            for box in bboxes
+            if box[0] != chosen_box[0]
+            or intersection_over_union(
+                torch.tensor(chosen_box[2:]),
+                torch.tensor(box[2:]),
+                box_format=box_format,
+            )
+            < iou_threshold
+        ]
+
+        bboxes_after_nms.append(chosen_box)
+
+    return bboxes_after_nms
